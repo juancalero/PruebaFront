7c15c77e0b93fdee45f802f851cab32e6a5f2369
Jorge Alonso <jorgealonsolopez77@gmail.com>
2021-06-11 17:34:11 +0200

    listado de favoritos funcional

--- prueba/src/app/app-routing.module.ts
+++ prueba/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { DetalleComponent } from './components/detalle/detalle.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
+import { PuntuadosComponent } from './components/puntuados/puntuados.component';
 
 
 const routes: Routes = [
   {path: '', component: BuscadorComponent},
-  {path: 'pelicula/:id', component: DetalleComponent}
+  {path: 'pelicula/:id', component: DetalleComponent},
+  {path: 'favoritos', component: FavoritosComponent},
+  {path: 'valoraciones', component: PuntuadosComponent}
 ];
 
 @NgModule({
--- prueba/src/app/components/favoritos/favoritos.component.ts
+++ prueba/src/app/components/favoritos/favoritos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
+import { FilmService } from 'src/app/service/film.service';
+import { FilmResponse } from '../../interfaces/filmDetailResponse';
 
 @Component({
   selector: 'app-favoritos',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritosComponent implements OnInit {
 
-  constructor() { }
+  listado: string[] = [];
+
+  constructor(private service:FilmService, private location: Location) { }
 
   ngOnInit(): void {
+
+    var fav = JSON.parse(localStorage.getItem("fav"));
+
+    fav.forEach(element => {
+      this.service.getPelicula(element).subscribe(
+      (response) => {                
+       this.listado.push(response.title);
+      }
+    )
+    });
   }
+
 
 }
